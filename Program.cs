cc7223802250f0d1d53062aa65e22c42608c6e89
rstefko <roman.stefko@eway-crm.com>
2024-05-24 10:54:18 +0200

    Initial version

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using System.Security.Policy;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("No arguments provided");
+                Environment.Exit(-1);
+                return;
+            }
+
+
+            if (!args[0].EndsWith(".exe") || !File.Exists(args[0]))
+            {
+                Console.Write("First parameter should be full path of the executable trigger.");
+                Environment.Exit(-1);
+                return;
+            }
+
+            RunInSandbox(args[0], args.Skip(1).ToArray());
+        }
+
+        static void RunInSandbox(string targetPath, string[] args)
+        {
+            AppDomainSetup setup = new AppDomainSetup();
+            setup.ApplicationBase = Path.GetDirectoryName(targetPath);
+
+            PermissionSet permissionSet = new PermissionSet(PermissionState.None);
+            permissionSet.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+            permissionSet.AddPermission(new FileIOPermission(FileIOPermissionAccess.AllAccess, setup.ApplicationBase));
+            permissionSet.AddPermission(new UIPermission(PermissionState.Unrestricted));
+            permissionSet.AddPermission(new WebPermission(PermissionState.Unrestricted));
+            permissionSet.AddPermission(new DnsPermission(PermissionState.Unrestricted));
+            permissionSet.AddPermission(new SocketPermission(PermissionState.Unrestricted));
+            permissionSet.AddPermission(new NetworkInformationPermission(PermissionState.Unrestricted));
+            permissionSet.AddPermission(new ReflectionPermission(PermissionState.Unrestricted));
+
+            string configFile = $"{targetPath}.config";
+            if (File.Exists(configFile))
+            {
+                setup.ConfigurationFile = configFile;
+            }
+
+            string applicationName = FileVersionInfo.GetVersionInfo(targetPath).ProductName;
+            AppDomain sandbox = AppDomain.CreateDomain("ExecutableTriggerSandbox", new Evidence(), setup, permissionSet);
+            sandbox.ExecuteAssembly(targetPath, args);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SandboxRunner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eWay System s.r.o.")]
+[assembly: AssemblyProduct("SandboxRunner")]
+[assembly: AssemblyCopyright("Copyright © eWay System s.r.o. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7f48512-dd79-46bf-8a08-144460ebeef4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
